ad7e0d7dca617e7789da9d775a766071b0dcd549
dansolodar <62340734+dansolodar@users.noreply.github.com>
2020-03-19 18:01:16 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ import { AlertBodyComponent } from './alert-body/alert-body.component';
 export class AppComponent  {  
   constructor(private dialog: MatDialog) {}
 
-
+  mockData={
+    title: 'חסימת הרשאה',
+    message: 'פעולתך לא נשמרה עקב בעייה במאגר',
+    image: '',
+    option1: 'פנה לתמיכה',
+    option2: 'הבנתי'
+  }
+  
   openDialog() {
       const dialogConfig = new MatDialogConfig();
       dialogConfig.disableClose=true;
       dialogConfig.panelClass='dialog';
+      dialogConfig.data=this.mockData;
       this.dialog.open(AlertBodyComponent, dialogConfig);
  }
 }
